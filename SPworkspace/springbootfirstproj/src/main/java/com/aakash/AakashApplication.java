59f19ba21e5d35e11c5e00bd36fb980e19fba4b1
Aakashrathod9617 <aakashrathod656156@gmail.com>
2024-03-19 19:30:01 +0530

    aakash Spring

---
+++ SPworkspace/springbootfirstproj/src/main/java/com/aakash/AakashApplication.java
@@ -0,0 +1,13 @@
+package com.aakash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AakashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AakashApplication.class, args);
+	}
+
+}
